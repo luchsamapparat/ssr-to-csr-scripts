9281ccd25bdf36a6d2f02a8fb82505d9758d5db1
Marvin Luchs <marvin@luchs.org>
2021-03-15 12:18:33 +0100

    feat: add github-linguist script

---
+++ github-linguist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+declare -a directories=(
+    "01-ssr"
+    "02-ssr-with-progressive-enhancement"
+    "03-ssr-with-partial-dom-updates"
+    "04-ssr-with-partial-csr"
+    "05-csr-with-spa"
+    "06-ssr-with-rehydration"
+)
+
+for directory in "${directories[@]}"
+do
+    cd $directory
+    echo $directory
+    github-linguist
+    echo ""
+    cd ..
+done
+

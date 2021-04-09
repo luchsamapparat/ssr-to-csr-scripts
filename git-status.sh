0b63bd88d956a71831cfb3dfb87e24956ede1edd
Marvin Luchs <marvin.luchs@holisticon.de>
2021-04-09 15:25:53 +0200

    feat: add git status script

---
+++ git-status.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -a directories=(
+    "01-ssr"
+    "02-ssr-with-progressive-enhancement"
+    "03-ssr-with-partial-dom-updates"
+    "04-ssr-with-partial-csr"
+    "05-csr-with-spa"
+    "06-ssr-with-rehydration"
+)
+
+for directory in "${directories[@]}"
+do
+    cd $directory
+    git status
+    cd ..
+done
